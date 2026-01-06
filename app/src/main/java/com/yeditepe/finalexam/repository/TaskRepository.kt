2be7a24245001ef71431eb5af817c194df46596b
ccansuball-bit <c.cansu.ballll@gmail.com>
2026-01-06 15:08:53 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -2,12 +2,17 @@ package com.yeditepe.finalexam.repository
 
 import com.yeditepe.finalexam.api.TaskApi
 import com.yeditepe.finalexam.model.Task
+import com.yeditepe.finalexam.model.TaskDto
 
 class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+        val taskDtos = api.getTasks()
         // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+        return taskDtos.map { dto ->
+            Task(id = dto.id, title = dto.title, isCompleted = dto.completed)
+
+        }
     }
 }
