42119a9c9c12bccd48b364ea8c4f10e94909b678
ccansuball-bit <c.cansu.ballll@gmail.com>
2026-01-06 15:08:23 +0300

    Update AppNavGraph.kt

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -1,8 +1,13 @@
 package com.yeditepe.finalexam.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.yeditepe.finalexam.ui.TaskDetailScreen
+import com.yeditepe.finalexam.ui.TaskListScreen
 
 @Composable
 fun AppNavGraph() {
@@ -15,7 +20,19 @@ fun AppNavGraph() {
     ) {
 
         // TODO 1: Add composable for "tasks"
+        composable("tasks") {
+            TaskListScreen(navController = navController)
+        }
 
         // TODO 2: Add composable for "taskDetail/{title}"
+        composable(
+            "taskDetail/{title}",
+            arguments = listOf(navArgument("title") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val title = backStackEntry.arguments?.getString("title")
+            if (title != null) {
+                TaskDetailScreen(title = title)
+            }
+        }
     }
 }
